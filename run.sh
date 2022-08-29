c6b457c468006629078fb8c388e6cb7c025c909a
batman004 <pathania.yuvraj@gmail.com>
2022-08-29 15:17:11 +0530

    add run script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 cd app
 
-docker-compose --env-file .env  up --build                                              
+docker-compose --env-file .env  up --build
+
+alembic init
+
+docker-compose run web alembic revision --autogenerate -m "First migration"
+
+docker-compose run web alembic upgrade head
